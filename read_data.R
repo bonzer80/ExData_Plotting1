2021e74c8fce1f49ed5659f2df8a1da23b197877
bonzer80 <arvind.ragunath@gmail.com>
2015-04-11 23:28:37 -0500

    Revert "Course Project 1"
    This reverts commit 728f7ec8818453fc060d4e29a9c3feb9dba815a1.

--- read_data.R
+++
@@ -1,7 +0,0 @@
-#Read data and convert date format
-mydata <- read.table("household_power_consumption.txt", sep=";", quote="\"",na.strings = "?",header=TRUE)
-mydata$Date <- as.Date(mydata$Date,"%d/%m/%Y")
-
-#Create subset of data for plotting
-dataforuse <- subset(mydata, Date >= "2007-02-01" & Date <="2007-02-02" )
-dataforuse[,"DateTime"]<-paste(dataforuse[,"Date"], dataforuse[,"Time"],sep=" ")
